5c5808dc62800ea4ac388fe0f2ad6b80d522a1cd
Sunil-Barik <sunilbarik1862@gmail.com>
2024-10-20 17:43:53 +0530

    Ready

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   // Set the launch date for the countdown
   let launchDate = new Date("2024-10-21T12:00:00").getTime();
-  
+  //hello
   // Start the countdown timer that calls the tick function every second
   let timer = setInterval(tick, 1000);
 
